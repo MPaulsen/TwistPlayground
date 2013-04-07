5bd3410fd43babc1c4343e5cb5e403589ff4c62e
rsf2 <russ.frisch@me.com>
2013-04-07 16:04:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwistPlayground::Application.config.secret_token = '4a74454920aa8d40ba5a221a01eacaa3e0e36ad09c48234cd2c015767a7deef65adf49b38b4c1474e89431d124c31d58d61eba60876957bc977a5dfae8787f54'
